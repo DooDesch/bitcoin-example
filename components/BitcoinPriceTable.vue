d931d8828c755447362ebe1cfb288f834711544d
Doodesch <30825893+DooDesch@users.noreply.github.com>
2022-05-02 17:17:25 +0200

    Update text alignment

--- components/BitcoinPriceTable.vue
+++ components/BitcoinPriceTable.vue
@@ -8,21 +8,21 @@
           <thead>
             <tr>
               <th class="text-left">Currency</th>
-              <th class="text-left">Buy</th>
-              <th class="text-left">Sell</th>
-              <th v-if="showMoreData" class="text-left">15 minutes</th>
-              <th v-if="showMoreData" class="text-left">Last</th>
+              <th class="text-right">Buy</th>
+              <th class="text-right">Sell</th>
+              <th v-if="showMoreData" class="text-right">15 minutes</th>
+              <th v-if="showMoreData" class="text-right">Last</th>
             </tr>
           </thead>
           <tbody v-if="data">
             <tr v-for="(currency, name) in data" :key="name">
               <td>{{ name }}</td>
-              <td>{{ addSeperators(currency.buy) }}</td>
-              <td>{{ addSeperators(currency.sell) }}</td>
-              <td v-if="showMoreData">
+              <td class="text-right">{{ addSeperators(currency.buy) }}</td>
+              <td class="text-right">{{ addSeperators(currency.sell) }}</td>
+              <td v-if="showMoreData" class="text-right">
                 {{ addSeperators(currency['15m']) }}
               </td>
-              <td v-if="showMoreData">
+              <td v-if="showMoreData" class="text-right">
                 {{ addSeperators(currency.last) }}
               </td>
             </tr>
