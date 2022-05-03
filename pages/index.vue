4b7278602238f1e3eb2e478d39cb08edf2a716e7
Doodesch <30825893+DooDesch@users.noreply.github.com>
2022-05-03 20:09:38 +0200

    Fix dashboard element sizes

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <v-row justify="center" class="px-3">
-    <v-col cols="12" md="3">
+    <v-col cols="12" md="4">
       <bitcoin-balance
         :bitcoin-currencies="bitcoinCurrencies"
       ></bitcoin-balance>
     </v-col>
-    <v-col cols="12" md="9">
+    <v-col cols="12" md="8">
       <bitcoin-price-table :data="bitcoinCurrencies"></bitcoin-price-table>
     </v-col>
   </v-row>
